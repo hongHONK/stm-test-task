6dd234b7b74b7387d479a832d8fdf3f7b99e991d
Alexey Popov <alexnnmenter@gmail.com>
2025-02-14 22:59:31 +0300

    fix: clsx теперь работает корректно

--- src/utils/clsx.ts
+++ src/utils/clsx.ts
@@ -1,9 +1,9 @@
-type ClassName = string;
+type ClassName = string | undefined | false;
 
 function clsx(...classes: ClassName[]): string {
   if (classes.length === 0) return "";
 
-  return classes.join(" ");
+  return classes.filter((item) => typeof item === "string").join(" ");
 }
 
 export default clsx;
